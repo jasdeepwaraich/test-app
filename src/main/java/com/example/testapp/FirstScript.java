84bc791effff502c0fb5afd38829fa25359170ce
jasdeepwaraich <jasdeepw1718@gmail.com>
2020-12-11 16:12:21 +0530

    Thread example

---
+++ src/main/java/com/example/testapp/FirstScript.java
@@ -0,0 +1,32 @@
+package com.example.testapp;
+
+class FirstScript extends Thread{
+public void run()
+{
+	try
+	{
+	Thread.sleep(1000);
+	System.out.println("task");
+	}
+	catch(InterruptedException e)
+	{
+	throw new RuntimeException("Thread interrupted..."+e);
+	}
+
+}
+
+public static void main(String args[])
+{
+	FirstScript t1=new FirstScript();
+	t1.start();
+	try
+	{
+	t1.interrupt();
+	}
+	catch(Exception e)
+	{
+		System.out.println("Exception handled "+e);
+	}
+
+}
+}
