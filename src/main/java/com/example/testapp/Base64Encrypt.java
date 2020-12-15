978fd9cbc5c33c33fce47c7d2020fd59a7dd5cb8
jasdeepwaraich <jasdeepw1718@gmail.com>
2020-12-15 17:00:20 +0530

    Java 8 Base64 Encryption  ex

---
+++ src/main/java/com/example/testapp/Base64Encrypt.java
@@ -0,0 +1,19 @@
+package com.example.testapp;
+
+import java.util.Base64; 
+public class Base64Encrypt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a[]= {1,2,3,4};
+		Base64.Encoder b = Base64.getEncoder();
+		byte c[]=b.encode(a);
+		System.out.println("Encoded array: " + c);
+		byte d[]=new byte[20];
+		int e=b.encode(a,d);
+		System.out.println("Encoded array : " + d);
+		
+	
+	}
+
+}
